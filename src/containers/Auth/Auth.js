200845f35786e21cdc196ed7d5f3bbb04588236f
Sasha <ljutaev@gmail.com>
2018-10-24 09:58:05 +0300

    Page Auth + Input Component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react'
 import classes from './Auth.css'
+import Button from '../../components/Ui/Button/Button'
+import Input from '../../components/Ui/Input/Input'
 
 export default class Auth extends Component {
+  loginHandler = () => {
+
+  }
+  registerHandler = () => {
+    
+  }
+  submitHandler = event => {
+    event.preventDefault()
+  }
   render() {
     return (
       <div className={classes.Auth}> 
         <div>
           <h1>Авторизация</h1>
-          
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <Input 
+              label="Email"
+            />
+            <Input 
+              label="Пароль"
+              errorMessage="ERROR"
+            />
+            <Button
+              type="success"
+              onClick={this.loginHandler}>
+              Вайти
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}>
+              Зарегестрироваться
+            </Button>
+          </form>
         </div>
         
       </div>
