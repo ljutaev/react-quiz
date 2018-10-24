68b4e9bc4bae0debef40cd505096fcb350d1153f
Sasha <ljutaev@gmail.com>
2018-10-24 08:34:40 +0300

    Components + Router + Basics

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import classes from './Auth.css'
+
+export default class Auth extends Component {
+  render() {
+    return (
+      <div className={classes.Auth}> 
+        <div>
+          <h1>Авторизация</h1>
+          
+        </div>
+        
+      </div>
+    )
+  }
+}
